0fead7dfe76813f0168c3acbd5f13523e4ee8914
umutcanozer <mytheawest@gmail.com>
2022-09-19 16:08:26 +0300

    commit

---
+++ EnBüyükSayi/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1=50;
+		int sayi2=45;
+		int sayi3=55;
+		
+		if(sayi1>sayi2&& sayi1>sayi3){
+			System.out.println("En büyük sayı:"+ sayi1);
+		}else if(sayi2>sayi1 && sayi2>sayi3){
+			System.out.println("En büyük sayı:"+ sayi2);
+		}else {
+			System.out.println("En büyük sayı:"+ sayi3);
+		}
+
+	}
+
+}
