dfbefa0cb906a763dbbf6e628d1bc7bbbb0f83d8
umutcanozer <mytheawest@gmail.com>
2022-09-19 16:12:50 +0300

    commit

---
+++ Switch/src/Main.java
@@ -0,0 +1,34 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		char grade= 'F';
+		
+		switch(grade) {
+			case 'A':
+				System.out.println("Çok iyi: Geçtiniz");
+				break;
+				
+			case 'B':
+				System.out.println("İyi : Geçtiniz");
+				break;
+			
+			case 'C':
+				System.out.println("Ortalama : Geçtiniz");
+				break;
+				
+			case 'D':
+				System.out.println("Ortalama altı : Geçtiniz");
+				break;
+				
+			case 'F':
+				System.out.println("Geçemediniz");
+				break;
+				
+			default:
+				System.out.println("Geçersiz not girildi.");
+		}
+
+	}
+
+}
