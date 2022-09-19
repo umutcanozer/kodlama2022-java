0109baf1574abd9a9332fa94d76a241bfcd77585
umutcanozer <mytheawest@gmail.com>
2022-09-19 16:13:31 +0300

    commit

---
+++ While/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int i=1;
+		while(i<10) {
+			System.out.println(i);
+			i+=2;
+		}
+		System.out.println("Döngü bitti");
+
+	}
+
+}
