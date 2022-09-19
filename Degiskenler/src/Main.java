e69b9b2444a28de46756450dc04b0721f0ca07a3
umutcanozer <mytheawest@gmail.com>
2022-09-19 16:07:17 +0300

    commit

---
+++ Degiskenler/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayi=10;
+		String sayimYapilan="Öğrenci sayısı:";
+		System.out.println(sayimYapilan + ogrenciSayi);
+		System.out.println(sayimYapilan + ogrenciSayi);
+		System.out.println(sayimYapilan + ogrenciSayi);
+
+	}
+
+}
