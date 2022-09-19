1ba917c51043dc0ee74b715c25cb5c23cf962d9c
umutcanozer <mytheawest@gmail.com>
2022-09-19 16:13:16 +0300

    commit

---
+++ VeriTipi/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//tam sayı int short long
+		int sayi=12;
+		//2 seçenek: false or true
+		boolean durum=true;
+		//ondalık double float
+		double sayi1= 12.5;
+		//karakter
+		char karakter='U';
+		//karakter topluluğu
+		String ülke="Türkiye";
+
+	}
+
+}
