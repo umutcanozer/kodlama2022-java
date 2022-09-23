9f1e41fd4d2846ea2e7ced3dfb4707b82a9ab78e
umutcanozer <mytheawest@gmail.com>
2022-09-23 19:10:07 +0300

    Second Week

---
+++ methodOverloading/src/methodOverloading/Main.java
@@ -0,0 +1,12 @@
+package methodOverloading;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Math math = new Math();
+		System.out.println(math.addition(2, 3));
+		System.out.println(math.addition(2, 3, 5));
+
+	}
+
+}
