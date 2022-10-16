49f1a3cf6c966d59ca04f23b394c08527d1cb16e
umutcanozer <mytheawest@gmail.com>
2022-10-16 19:01:47 +0300

    Third Week

--- Interfaces/src/Main.java
+++ Interfaces/src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 
 	public static void main(String[] args) {
-		ICustomerDal customerDal = new OracleCustomerDal(); 
-
+		CustomerManager customerManager = new CustomerManager(new OracleCustomerDal()); 
+		customerManager.add();
 	}
 
 }
