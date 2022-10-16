7a03cc27ec0bcff474831744dd2454a47a43ace3
umutcanozer <mytheawest@gmail.com>
2022-10-16 17:08:41 +0300

    Third Week

---
+++ Overriding/src/BaseKrediManager.java
@@ -0,0 +1,6 @@
+
+public class BaseKrediManager {
+	public double calculate(double amount) { // final kelimesiyle override'dan koruyabilirsin
+		return amount * 1.18;
+	}
+}
---
+++ Overriding/src/OgrenciKrediManager.java
@@ -0,0 +1,6 @@
+
+public class OgrenciKrediManager extends BaseKrediManager{
+	public double calculate(double amount) {
+		return amount * 1.09 ;
+	}
+}
